7e04788d34f53fdfec89c2627138c5001fdd3cfa
Lukas Kristensen <lukadk00@gmail.com>
2024-10-28 12:22:45 +0100

    Added show and add database

--- inventory-management/src/app/app.component.ts
+++ inventory-management/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { StockOverviewComponent } from './stock-overview/stock-overview.component';
+import { StockItemFormComponent } from './stock-item-form/stock-item-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <div class="container">
+      <h1>Inventory Management</h1>
+      <app-stock-item-form></app-stock-item-form>
+      <hr>
+      <app-stock-overview></app-stock-overview>
+    </div>
+  `,
+  imports: [StockItemFormComponent, StockOverviewComponent], // Import both components here
 })
-export class AppComponent {
-  title = 'inventory-management';
-}
+export class AppComponent {}
